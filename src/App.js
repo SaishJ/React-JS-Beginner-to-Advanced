75de59bc80822bb37c23b7aa597a814394a1ec1c
SaishJ <saishjagtap0003@gmail.com>
2022-08-05 18:18:09 +0530

    :package: Class Component
    Class Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import FirstComponent from "./components/Greet"; //Default Export
 //import Greet from "./components/Greet"; //Named Export
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
       <FirstComponent />
+      <Welcome />
     </div>
   );
 }
