7ae573ec1a58e3298ed81145ba1dfc1ba30f11e4
SaishJ <saishjagtap0003@gmail.com>
2022-08-10 16:14:09 +0530

    :speech_balloon: State
    State

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
+      <Message />
       {/* Functional Components props */}
-      <Greet name="Saish" nickName="SJ" />
+      {/* <Greet name="Saish" nickName="SJ" />
       <Greet name="Pratik" nickName="PZ" />
-      <Greet name="Prafulla" nickName="Papya" />
+      <Greet name="Prafulla" nickName="Papya" /> */}
       {/* Class Component props */}
-      <Welcome name="Shivam" nickName="SA">
+      {/* <Welcome name="Shivam" nickName="SA">
         <button>Click Me</button>
-      </Welcome>
+      </Welcome> */}
       {/* <Hello /> */}
     </div>
   );
